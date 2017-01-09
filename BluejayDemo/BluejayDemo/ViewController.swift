85c13d7ea201c427b24c9759f6e8193cbeca256b
Jeremy Chiang <jeremy@steamclock.com>
2017-01-09 13:24:20 -0800

    Add BluejayDemo project for testing usage of Bluejay's API

---
+++ BluejayDemo/BluejayDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluejayDemo
+//
+//  Created by Jeremy Chiang on 2017-01-09.
+//  Copyright © 2017 Steamclock Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
