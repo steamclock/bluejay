dd92e4bfee2a17a3572bc97ceaad48c13b89b2b8
Jeremy Chiang <jeremy@steamclock.com>
2017-05-26 11:46:47 -0700

    Add an extension to Data for easier reading of bytes into concrete and primitive types

---
+++ Bluejay/Bluejay/Data+Extractable.swift
@@ -0,0 +1,17 @@
+//
+//  Data+Extractable.swift
+//  Bluejay
+//
+//  Created by Jeremy Chiang on 2017-05-26.
+//  Copyright © 2017 Steamclock Software. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    
+    func extract<T>(start: Int, length: Int) -> T {
+        return self.subdata(in: start..<start+length).withUnsafeBytes { $0.pointee }
+    }
+    
+}
