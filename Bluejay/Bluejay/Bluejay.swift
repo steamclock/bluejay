aa176863672d17e76773326370e7918c113c636c
Jeremy Chiang <jeremy@steamclock.com>
2017-07-07 16:01:25 -0700

    Use the isConnected property instead when figuring out whether shouldAutoReconnect needs to overriden

--- Bluejay/Bluejay/Bluejay.swift
+++ Bluejay/Bluejay/Bluejay.swift
@@ -670,7 +670,7 @@ extension Bluejay: CBCentralManagerDelegate {
          If Bluejay is not even connected when didDisconnectPeripheral is called (can happen when a pending connection is cancelled), Bluejay should not try to auto reconnect. Also, do not override if shouldAutoReconnect is already explicitly set to false from cancelEverything or from a manual disconnect.
          */
         if shouldAutoReconnect {
-            shouldAutoReconnect = connectedPeripheral != nil
+            shouldAutoReconnect = isConnected
         }
         
         let peripheralString = peripheral.name ?? peripheral.identifier.uuidString
