b67d38e1402347bb2d3b67899c93340c6e09eef9
Jeremy Chiang <jeremy@steamclock.com>
2017-07-24 13:23:43 -0700

    Add missing inline documentation to a few structs
    - CharacteristicIdentifier
    - ServiceIdentifier
    - DataPadding
    - PeripheralIdentifier

--- Bluejay/Bluejay/DataPadding.swift
+++ Bluejay/Bluejay/DataPadding.swift
@@ -8,15 +8,22 @@
 
 import Foundation
 
-/// Helper object to create padding in packets being constructed.
+/// Helper object that can create empty data to be used as padding in packet construction.
 public struct DataPadding: Sendable {
     
+    /// The number of bytes to be used for padding.
     private var amount: Int
     
+    /**
+     Create empty data.
+     
+     - Parameter amount: number of bytes.
+     */
     public init(_ amount: Int) {
         self.amount = amount
     }
     
+    /// This function is required to conform to the `Sendable` protocol, which allows Bluejay to serialize `DataPadding` when performing write-related operations.
     public func toBluetoothData() -> Data {
         return Data(count: amount)
     }
--- Bluejay/Bluejay/ServiceIdentifier.swift
+++ Bluejay/Bluejay/ServiceIdentifier.swift
@@ -12,12 +12,15 @@ import CoreBluetooth
 /// A wrapper for CBUUID specific to a service to help distinguish it from a CBUUID of a charactersitc.
 public struct ServiceIdentifier {
     
+    /// The CBUUID of this service.
     public private(set) var uuid: CBUUID
     
+    /// Create a ServiceIdentifier using a string. Please supply a valid 128-bit UUID, or a valid 16 or 32-bit commonly used UUID.
     public init(uuid: String) {
         self.uuid = CBUUID(string: uuid)
     }
     
+    /// Create a ServiceIdentifier using a CBUUID.
     public init(_ uuid: CBUUID) {
         self.uuid = uuid
     }
